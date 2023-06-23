6ab869331c41a18dc94b99a0be91e2c314207613
chaparroagustin <chaparroagustin91@hotmail.com>
2023-06-23 16:28:39 -0300

    Actualizacion de practicas del dia

--- Objetos/app.js
+++ Objetos/app.js
@@ -1,3 +1,62 @@
-let miLibro = {
-    
-}
+///OBJETOS
+
+
+// let miLibro = {
+//     titulo:'El libro de JavaScript',
+//     autor: 'Agustin',
+//     paginas: 400,
+//     publicado: false
+// };
+
+// console.log(miLibro.titulo); //uso comun de los OBJETOS
+// console.log(`${miLibro.titulo} creado por ${miLibro.autor}`);
+// miLibro.paginas = 500;
+// console.log(miLibro.paginas);
+
+//Ejercicio, crear un objeto persona que tenga las siguientes props:
+//nombre,edad, ciudad
+//cambiar edad
+//Mostrar datos de la persona
+
+// let persona ={
+//     nombre: "Agustin",
+//     edad: 31,
+//     ciudad: "Buenos Aires"
+// };
+// persona.edad = 32;
+// console.log(`${persona.nombre} tiene ${persona.edad} años, y vive en la ciudad de ${persona.ciudad}`);
+
+
+// let miLibroA = {
+//     titulo:'El libro de JavaScript',
+//     autor: 'Agustin',
+//     paginas: 400,
+//     publicado: false
+// };
+
+// let miLibro = {
+//     titulo:'Programacion en PHP',
+//     autor: 'Agustin',
+//     paginas: 700,
+//     publicado: true
+// };
+
+// let getResumen = (libro) => {
+//     return {
+//         resumen: `${libro.titulo} creado por ${libro.autor}`,
+//         resumenPaginas: `${libro.titulo} tiene ${libro.paginas} paginas.`
+//     }
+// };
+
+// console.log(getResumen(miLibro).resumen);
+// console.log(getResumen(miLibro).resumenPaginas);
+
+// console.log(getResumen(miLibroA).resumen);
+// console.log(getResumen(miLibroA).resumenPaginas);
+
+
+//-----------------------------------o----------------------------------
+
+//REFERENCIA DE OBJETOS
+
+
