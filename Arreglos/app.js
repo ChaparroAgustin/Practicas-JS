723a2bc4cd83cc72cb0a05c909f2552d666b89fd
chaparragustin <chaparroagustin91@hotmail.com>
2023-08-31 19:27:30 -0300

    hola

---
+++ Arreglos/app.js
@@ -0,0 +1,22 @@
+//////Arreglos en ES6/////
+
+
+const ceviche = ['pescado','marisco', 'cebolla'];
+
+// console.log(ceviche);
+// ceviche[0] = 'papa';
+// console.log(ceviche);
+
+
+// ceviche.splice(1,0, 'Limon');
+// console.log(ceviche);
+
+// ceviche.forEach(function(item, index) {
+//     console.log(index)
+//     console.log(item)
+
+// });
+
+console.log(ceviche.indexOf('cebolla'));
+
+
